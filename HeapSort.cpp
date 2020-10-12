8ce859236869cd1385b4dd4cf6d74fc398ad87f7
YosephAhmed <yosephahmed@aol.com>
2020-10-12 17:48:51 -0400

    feat: function declarations and skeleton

---
+++ HeapSort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void max_heapify(vector<int>& arr, int i, int size) {
+	// do something
+}
+
+void build_max_heap(vector<int>& arr) {
+	// do something
+}
+
+void heapSort(vector<int>& arr) {
+	// do something
+}
