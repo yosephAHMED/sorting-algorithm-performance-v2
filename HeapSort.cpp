cad678fd3d0f0b313b3a87cc6d6f4a07e9b8ad2c
YosephAhmed <yosephahmed@aol.com>
2020-10-12 17:56:27 -0400

    feat: max_heapify implementation

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -5,7 +5,23 @@
 using namespace std;
 
 void max_heapify(vector<int>& arr, int i, int size) {
-	// do something
+	int largestVal;
+	int l = (2 * i) + 1;
+	int r = (l + 1);
+
+    if (l < size && arr[l] > arr[i])
+        largestVal = l;
+    else
+        largestVal = i;
+
+    if (r < size && arr[r] > arr[largestVal])
+        largestVal = r;
+
+    if (largestVal != i)
+    {
+        swap(arr[i], arr[largestVal]);
+        max_heapify(arr, largestVal, size);
+    }
 }
 
 void build_max_heap(vector<int>& arr) {
